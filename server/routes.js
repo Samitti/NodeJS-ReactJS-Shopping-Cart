0c98c6d7f471cb1482024534f44313e8b787dc79
Samitti <samuelteweldebrhan@gmail.com>
2021-09-24 13:32:46 +0300

    create custom checout hook and purchase cart with stripe checkout

--- server/routes.js
+++ server/routes.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const express = require("express");
 const products = require("./products.json");
+const stripe = require('stripe')(process.env.STRIPE_API_SECRET);
+const {validateCartItems} = require('use-shopping-cart/src/serverUtil')
 
 module.exports = function getRoutes() {
   const router = express.Router();
 
   router.get('/products', getProducts);
   router.get('/products/:productID', getProduct);
+
+  router.post('/checkout-sessions', createCheckoutSession);
+
   return router;
 };
 
@@ -25,5 +30,33 @@ function getProduct(req, res) {
     res.status(200).json({ product })    
   } catch (error) {
     res.status(404).json({ statusCode: 404, message: error.message });
+  }
+}
+
+async function createCheckoutSession(req, res) {
+  try{
+    const cartItems = req.body;
+    const line_items = validateCartItems(products, cartItems);
+
+    const origin = process.env.NODE_ENV === 'production' ? req.headers.origin : 'http://localhost:3000' 
+
+    const params = {
+      submit_type: "pay",
+      payment_method_types: ['card'],
+      billing_address_collection: 'auto',
+      shipping_address_collection: {
+        allowed_countries: ["US", "CA"]
+      },
+      line_items,
+      success_url: `${origin}/result?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: origin,
+      mode: 'payment',
+    }
+
+    const checkoutSession = await stripe.checkout.sessions.create(params);
+
+    res.status(200).json(checkoutSession);
+  } catch(error) {
+    res.status(500).json({ statusCode: 500, message: error.message })
   }
 }
